fb0604ee6c0d2a855e34739711890625e2f79e03
PotriRathna <88546064+PotriRathna@users.noreply.github.com>
2021-08-09 09:35:18 +0200

    Update Check_number.scala

--- Check_number.scala
+++ Check_number.scala
@@ -13,25 +13,15 @@ object Check_number extends App{
   println("Enter number 1 and number 2 : ")
   val number1 =scala.io.StdIn.readInt()
   val number2 =scala.io.StdIn.readInt()
-  val diff1 = 100 - number1
-  val diff2 = 100 - number2
-  if(number1 == number2)
-    println("Both number are equal ")
-  else if (diff1<0 && diff2>0)    // if number1 is greater than 100
-    println(s"$number2 is close to 100")
-  else if (diff2<0 && diff1>0)    // if number2 is greater than 100
-    println(s"$number1 is close to 100")
-  else if(diff2<0 && diff1<0)   // if both number1 and number 2 are greater than 100
-    {
-      if(diff1 > diff2)
-        println(s"$number1 is close to 100")
-      else
-        println(s"$number2 is close to 100")
-    }
-  else if(diff1<diff2)     // if both number1 and number 2 are less than 100
-    println(s"$number1 is close to 100")
-  else
-    println(s"$number2 is close to 100")
+  println("The closest number to 100 :" +test(number1,number2))
 
+  def test(x: Int, y: Int) = {
+    val n = 100
+    val val1 = scala.math.abs(x - n)
+    val val2 = scala.math.abs(y - n)
+    if (val1 == val2) 0
+    else if (val1 < val2) x
+    else y
+  }
 
 }
