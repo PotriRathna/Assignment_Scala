9421086a42bb45ab78086abe4f66f07ab976e5db
PotriRathna <88546064+PotriRathna@users.noreply.github.com>
2021-08-09 10:00:36 +0200

    Add files via upload

---
+++ Happyornot.scala
@@ -0,0 +1,25 @@
+package Potri
+/*
+: Write a Scala program to check whether a specified character is happy or not.
+ A character is happy when the same character appears to its left or right in a string.
+Input  Choose a number and specify that as a happy character
+Expected Output  Do the factorial to reach that number,
+if the number is achieved factorially, return “happy”, else return “not happy”
+
+ */
+object Happyornot extends App{
+
+  println("Enter the String : ")
+  val string1 = scala.io.StdIn.readLine()
+  println(fact(string1,0))
+  def fact(str:String,c:Int):String=
+    {
+      val len = string1.length
+      if(str.charAt(c)==str.charAt(c+1)) "Happy"
+      else
+        {
+          if(c<len-2) fact(str,c+1)
+          else "unhappy"
+        }
+      }
+}
